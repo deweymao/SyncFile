8c3765c6b92bb74d33b0fd011a16b518ad384f75
deweymao <deweymao@gmail.com>
2020-02-02 20:16:55 +0800

    read json

--- fileSync.py
+++ fileSync.py
@@ -2,12 +2,13 @@
 
 import os
 import hashlib
+import json
 
 # create all file directory(exclude root) and md5 of path
 def creatFileMD5Dict(root,path,listPathDir,dictFileMD5):
 	for name in os.listdir(path):
 		if name=="$RECYCLE.BIN" or name=="System Volume Information": 
-			continue; 
+			continue
 		childPath = path+os.sep+name
 		if os.path.isdir(childPath):
 			listPathDir.append(childPath[len(root):])
@@ -66,6 +67,17 @@ def syncFile(pathSource,pathTarget,dictSourceFileMD5,dictTargetFileMD5):
 						copyFile(pathTarget+tFile,pathSource+tFile)
 			else:
 				copyFile(pathTarget+tFile,pathSource+tFile)
+# read json file
+def readJson():
+	jsonPath = os.getcwd()+os.sep+'path.json'	
+	# print(jsonPath)
+	if os.path.isfile(jsonPath):
+		with open(jsonPath,'r') as f:
+			pathJson = json.load(f)
+			pathData = pathJson['path']
+			# print(pathData)
+			return pathData
+
 
 # main entrance
 def autoFileSync():
@@ -76,22 +88,28 @@ def autoFileSync():
 	
 	print('******************************** AUTO FILE SYNCHRONIZATION ********************************')
 	print('FUNCTION: SYNCHRONIZE TARGET PATH TO SOURCE PATH.')
-	path_source = input('Please input your source path(F:\\\\source): ')
-	path_target = input('Please input your target path(//192.168.0.101/target): ')
-	if os.path.isdir(path_source):
-		if os.path.isdir(path_target):
-			creatFileMD5Dict(path_source,path_source,listSourceDir,dictSourceFileMD5)
-			print('Source Directory Count: ',len(listSourceDir))
-			print('Source File Count: ',len(dictSourceFileMD5))
-			creatFileMD5Dict(path_target,path_target,listTargetDir,dictTargetFileMD5)
-			print('Target Directory Count: ',len(listTargetDir))
-			print('Target File Count: ',len(dictTargetFileMD5))
-			syncDir(path_source,path_target,listSourceDir,listTargetDir)
-			syncFile(path_source,path_target,dictSourceFileMD5,dictTargetFileMD5)
+	# path_source = input('Please input your source path(F:\\\\source): ')
+	# path_target = input('Please input your target path(//192.168.0.101/target): ')
+	pathData = readJson()
+	for path in pathData:
+		path_source = path['source']
+		path_target = path['target']
+		print("Source Directory: ", path_source)
+		print("Target Directory: ", path_target)
+		if os.path.isdir(path_source):
+			if os.path.isdir(path_target):
+				creatFileMD5Dict(path_source,path_source,listSourceDir,dictSourceFileMD5)
+				print('Source Directory Count: ',len(listSourceDir))
+				print('Source File Count: ',len(dictSourceFileMD5))
+				creatFileMD5Dict(path_target,path_target,listTargetDir,dictTargetFileMD5)
+				print('Target Directory Count: ',len(listTargetDir))
+				print('Target File Count: ',len(dictTargetFileMD5))
+				syncDir(path_source,path_target,listSourceDir,listTargetDir)
+				syncFile(path_source,path_target,dictSourceFileMD5,dictTargetFileMD5)
+			else:
+				print('[error] : target path error!')
 		else:
-			print('[error] : target path error!')
-	else:
-		print('[error] : source path error!')
+			print('[error] : source path error!')
 	print('******************************** AUTO FILE SYNCHRONIZATION ********************************')
 
 autoFileSync()
